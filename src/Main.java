1022b1dfe9a44fd3ad9383f826bc460fe80f04f6
adriana.nogueira <adriana.nogueira@zup.com.br>
2024-12-10 13:24:28 -0300

    Create BubbleSort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+            System.out.println(vetor[i]);
+        }
+        //BUBBLE SORT
+        int aux;
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[i] > vetor[j]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor[j] = aux;
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println("Vetor ordenado");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
