d8940b4c89c9c77001c9a9dc8c5f6cf6a075001f
Joaoapbrito1 <joao.brito@zup.com.br>
2024-12-10 13:58:04 -0300

    Feat: Implementa o Loop externo do Bubble Sorte.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int[] v = {7, 5, 10, 6, 8};
 
+        // Bubble sort
+        for (int i = 0; i < v.length - 1; i++) {
+
+        }
+
     }
 }
